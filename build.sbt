e8b9e895c47b7c37bcbf398b318b5fd89d6fbf03
Srijan Bhushan <srijanbhushan@Srijans-MacBook-Air.local>
2019-12-15 02:52:08 -0700

    adding changes to main app and built.dbt

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.3.2",
-  "org.apache.spark" %% "spark-sql" % "2.3.2"
+  "org.apache.spark" %% "spark-core" % "2.4.4",
+  "org.apache.spark" %% "spark-sql" % "2.4.1",
+  "org.apache.spark" %% "spark-mllib" % "2.4.0"
 )
--- src/main/scala/kickOffApp.scala
+++ src/main/scala/kickOffApp.scala
@@ -1,14 +1,22 @@
-import org.apache.spark.{SparkContext, SparkConf}
+import java.nio.file.Paths
+
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.image.ImageSchema._
+import org.apache.spark.sql._
 
 object kickOffApp {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder().master("local").getOrCreate()
+    val spark = SparkSession.builder().appName("SparkJob").master("local").getOrCreate()
+
     import spark.implicits._
 
     val dummyDf = Seq(Array[Int](1,2,3)).toDF()
+
+    dummyDf.write.parquet("s3://")
     dummyDf.show()
+
   }
 }
